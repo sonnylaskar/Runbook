59cf2dcf1bf4ad487a8ade49943ee89525848255
Sonny Laskar <sonnylaskar@gmail.com>
2016-12-17 22:20:36 +0530

    Create Apply_Same_Function_to_many_Columns.R

---
+++ R/Apply_Same_Function_to_many_Columns.R
@@ -0,0 +1,6 @@
+# How to apply the same function on many columns of a data table
+dt <- data.table(a = 1:3, b = 1:3, d = 1:3)
+cols <- c("a", "b")
+n <- 1 #Set it accordingly based on how many cores you want to use
+#Objective: Multiple all the columns by 2
+df[ , (cols) := mclapply(.SD, function(x) { x*2 }, mc.cores = n), .SDcols = cols]
