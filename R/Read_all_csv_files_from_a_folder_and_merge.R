0b0543a08e45f33f4018f252fda34c55ca962f4a
Sonny Laskar <SonnyL@microland.com>
2019-10-08 20:58:15 +0530

    added variable name in dplyr

--- R/Read_all_csv_files_from_a_folder_and_merge.R
+++ R/Read_all_csv_files_from_a_folder_and_merge.R
@@ -1,3 +1,5 @@
 #How to read many csv files present in a directory and merge them:
+library(readr)
+library(dplyr)
 file_names <- dir() 
-your_data_frame <- do.call(rbind,lapply(file_names,read.csv))
+your_data_frame <- do.call(bind_rows,lapply(file_names,read_csv))
